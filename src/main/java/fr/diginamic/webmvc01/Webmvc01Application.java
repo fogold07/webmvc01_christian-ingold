d284cb1bb783578c9df9c4ae877069bab4be309c
INGOLD <fogold07@gmail.com>
2022-01-28 12:21:07 +0100

    Ajout des i18n gestion du FR

--- src/main/java/fr/diginamic/webmvc01/Webmvc01Application.java
+++ src/main/java/fr/diginamic/webmvc01/Webmvc01Application.java
@@ -2,6 +2,9 @@ package fr.diginamic.webmvc01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class Webmvc01Application {
@@ -10,4 +13,17 @@ public class Webmvc01Application {
 		SpringApplication.run(Webmvc01Application.class, args);
 	}
 
+	/**
+	 * Configuration pour le chargement des messages Intenationaux
+	 * messages.properties
+	 * 
+	 * @return
+	 */
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("i18n/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
 }
